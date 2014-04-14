c0c20b795447a8b307f37c4ad550f74d07b2f4ad
joshua.davis <joshua.davis@zeroengine.io>
2014-04-14 14:49:58 -0700

    Added all the files I forgot to add...

---
+++ Content/RunTest.py
@@ -0,0 +1,25 @@
+import Zero
+import Events
+import VectorMath
+import Action
+
+class RunTest:
+    def Initialize(self):
+        if(Zero.Environment.GetParsedArgument("RunTest")):
+            print("init")
+            self.RunCommand(Zero.Editor)
+        
+    def Execute(self, Editor):
+        self.RunCommand(Editor)
+        
+    def RunCommand(self, Editor):
+        seq = Action.Sequence(Editor.Actions)
+        Action.Delay(seq, 1.0)
+        Action.Call(seq, self.PlayGame)
+        Action.Delay(seq, 5.0)
+        Action.Call(seq, Zero.Engine.Terminate)
+
+    def PlayGame(self):
+        Zero.Editor.ExecuteCommand("PlayGame")
+
+Zero.RegisterCommand("RunTest", RunTest)
